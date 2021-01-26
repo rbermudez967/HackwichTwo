8534b5b5e8ec2d8b7c306e507affe6c4cc6d6344
rbermudez967 <77324380+rbermudez967@users.noreply.github.com>
2021-01-26 11:39:28 -1000

    Part 8 January 26 2021
    Completed Parts 1 - 8

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -15,6 +15,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //part 7 - we are going to create our first IB action
+    
+    
+    @IBAction func touchMeButtonPressed(_ sender: Any) {
+    
+ 
+    //part 7 number 5
+    print("Oh My Gosh")
 
+        
+    //part 8 - adding our own button
+        
+    }
+    
+    @IBAction func freddyButtonPressed(_ sender: Any) {
+        print("I got this!")
+    }
+    
 }
-
